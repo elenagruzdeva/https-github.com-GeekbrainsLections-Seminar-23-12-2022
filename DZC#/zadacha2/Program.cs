9308f151a4ad013ef37f38a5429ed19eca185f9c
Elena <novikovalenockam@mail.ru>
2023-01-29 10:36:41 +0300

    reshenie 2 zadachi

---
+++ DZC#/zadacha2/Program.cs
@@ -0,0 +1,13 @@
+﻿// //Задача Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.2, 3, 7 -> 7
+
+
+Console.WriteLine("vvedite 3 chisla");
+int numberA = Convert.ToInt32 (Console.ReadLine());
+int numberB= Convert.ToInt32 (Console.ReadLine());
+int numberC = Convert.ToInt32 (Console.ReadLine());
+
+
+For (numberA > numberB > numberC );
+{
+    System.Console.WriteLine (numberA);
+}
